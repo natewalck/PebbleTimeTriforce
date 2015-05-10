e5425f37fc4fcdb0386251cfe81ca4ba1e70ce09
Nate Walck <nate.walck@gmail.com>
2015-05-09 22:52:47 -0700

    Removed some stuff

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
-static TextLayer *s_weather_layer;
 
 static uint8_t battery_percent;
 static bool battery_state;
@@ -165,17 +164,6 @@ static void main_window_load(Window *window) {
   // Create second font, apply it and add to Window
   s_status_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
 
-  // Create temperature Layer
-  s_weather_layer = text_layer_create(GRect(12, 64, 132, 88));
-  text_layer_set_background_color(s_weather_layer, GColorClear);
-  text_layer_set_text_color(s_weather_layer, GColorBlack);
-  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-  text_layer_set_text(s_weather_layer, "Loading...");
-
-  // Set font for weather layer
-  text_layer_set_font(s_weather_layer, s_status_font);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
-
 
   // Create Date Layer
   s_date_layer = text_layer_create(GRect(8, 0, 128, 88));
@@ -205,73 +193,12 @@ static void main_window_unload(Window *window) {
 
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
-
-  // Destroy weather elements
-  text_layer_destroy(s_weather_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
-
-  // Get weather update every 30 minutes
-  if(tick_time->tm_min % 30 == 0) {
-    // Begin dictionary
-    DictionaryIterator *iter;
-    app_message_outbox_begin(&iter);
-
-    // Add a key-value pair
-    dict_write_uint8(iter, 0, 0);
-
-    // Send the message!
-    app_message_outbox_send();
-  }
 }
 
-
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  // Store incoming information
-  static char temperature_buffer[8];
-  static char conditions_buffer[32];
-  static char weather_layer_buffer[32];
-
-  // Read first item
-  Tuple *t = dict_read_first(iterator);
-
-  // For all items
-  while(t != NULL) {
-    // Which key was received?
-    switch(t->key) {
-    case KEY_TEMPERATURE:
-      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dF", (int)t->value->int32);
-      break;
-    case KEY_CONDITIONS:
-      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-      break;
-    default:
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-      break;
-    }
-
-    // Look for next item
-    t = dict_read_next(iterator);
-  }
-
-  // Assemble full string and display
-  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
-  text_layer_set_text(s_weather_layer, weather_layer_buffer);
-}
-
-static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-}
-
-static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
 
 static void init() {
   // Create main Window element and assign to pointer
@@ -292,14 +219,6 @@ static void init() {
   // Subscribe to the Battery State Service
   battery_state_service_subscribe(battery_handler);
 
-  // Register callbacks
-  app_message_register_inbox_received(inbox_received_callback);
-  app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-  app_message_register_outbox_sent(outbox_sent_callback);
-
-  // Open AppMessage
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void deinit() {
