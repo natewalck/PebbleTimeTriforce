f2c613bb7954367fea09e2d1f452f52b1d3152e4
Nate Walck <nate.walck@gmail.com>
2015-12-03 10:35:42 -0500

    Changed font size and started battery code

--- src/main.c
+++ src/main.c
@@ -2,11 +2,14 @@
 
 static Window *s_main_window;
 static GBitmap *s_bitmap;
-static BitmapLayer *s_bitmap_layer;
+static BitmapLayer *s_battery_layer;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
 
 static GFont s_time_font;
+
+static uint8_t battery_percent;
+static bool battery_state;
 
 static void update_time() {
   // Get a tm structure
@@ -35,6 +38,63 @@ static void update_time() {
 //   text_layer_set_text(s_date_layer, date_text);
 }
 
+static void battery_handler(BatteryChargeState new_state) {
+  // Write to buffer and display
+  battery_percent = new_state.charge_percent;
+  battery_state = new_state.is_charging;
+
+  GBitmap *battery_image;
+
+  if (battery_state)
+  {
+    battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERYCHARGING);
+  }
+  else
+  {
+    switch(battery_percent) {
+      case 100:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY100);
+        break;
+      case 90:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY090);
+        break;
+      case 80:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY080);
+        break;
+      case 70:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY070);
+        break;
+      case 60:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY060);
+        break;
+      case 50:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY050);
+        break;
+      case 40:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY040);
+        break;
+      case 30:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY030);
+        break;
+      case 20:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY020);
+        break;
+      case 10:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY010);
+        break;
+      case 0:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY000);
+        break;
+      default:
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY100);
+        break;
+      }
+  }
+
+  bitmap_layer_set_bitmap(s_battery_layer, battery_image);
+  layer_mark_dirty(bitmap_layer_get_layer(s_battery_layer));
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
@@ -49,29 +109,29 @@ static void main_window_load(Window *window) {
   s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY100);
 
   // Create bitmap layer within the bounds of the main window layer
-  s_bitmap_layer = bitmap_layer_create(bounds);
+  s_battery_layer = bitmap_layer_create(bounds);
   // Apply the battery bitmap to the bitmap layer
-  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+  bitmap_layer_set_bitmap(s_battery_layer, s_bitmap);
   // Align the bitmap the bottom of the bitmap layer bounds
-  bitmap_layer_set_alignment(s_bitmap_layer, GAlignBottom);
+  bitmap_layer_set_alignment(s_battery_layer, GAlignBottom);
   // Set correct compositing mode based upon pebble type
 #ifdef PBL_PLATFORM_APLITE
-  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpAssign);
+  bitmap_layer_set_compositing_mode(s_battery_layer, GCompOpAssign);
 #elif PBL_PLATFORM_BASALT
-  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+  bitmap_layer_set_compositing_mode(s_battery_layer, GCompOpSet);
 #endif
   // Add the battery bitmap layet to the main window layer
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_battery_layer));
   
   // Create GFont for Time Layer
-  s_time_font = fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS);
+  s_time_font = fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS);
   // Figure out dimensions for Time Layer
   GSize time_max_size = graphics_text_layout_get_content_size("00:00", s_time_font, GRect(7, 41, 132, 88), GTextOverflowModeWordWrap , GTextAlignmentCenter);
   
   // Default padding values
-  int bg_header_size = 5;
+  int bg_header_size = 1;
   int bg_window_size = 88;
-  int textlayer_padding = 5;
+  int textlayer_padding = 26;
 
   // Setup time text and layer
   int time_text_y = bg_header_size + (bg_window_size / 2) - (time_max_size.h / 2) - textlayer_padding;
@@ -87,6 +147,9 @@ static void main_window_load(Window *window) {
   
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  // Get the current battery level
+//   battery_handler(battery_state_service_peek());
   
   // Make sure the time is displayed from the start
   update_time();
@@ -94,7 +157,7 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   // Destroy the bitmap layer
-  bitmap_layer_destroy(s_bitmap_layer);
+  bitmap_layer_destroy(s_battery_layer);
   // Destroy the bitmap object
   gbitmap_destroy(s_bitmap);
   // Destroy TextLayer
@@ -120,6 +183,9 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Subscribe to the Battery State Service
+//   battery_state_service_subscribe(battery_handler);
 }
 
 static void deinit() {
